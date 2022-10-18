edbbad7223c1567edc52b87d0a5c5982b2d32b6c
OstelloFlavio <ostello.flavio@panettipitagora.edu.it>
2022-10-18 11:51:26 +0200

    fine calcolo.php

--- static/Backend/PHP/calcolo.php
+++ static/Backend/PHP/calcolo.php
@@ -4,5 +4,9 @@ if(isset($_POST["submit"]))
 	$giorni = $_POST["giorni"];
 	$ore = (Float)$_POST["ore"];
 	$compenso = (Float)$_POST["compenso"];
+
+	$tot = $giorni * $ore * $compenso;
+
+	echo("Guadagnerai $tot&euro; lavorando $giorni giorni al compenso di $compenso&euro;/h");
 }
 ?>
